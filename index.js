f7a45245e101ad830b6f987dec79226fb51d8618
S. M. Sujan <smsujon50@gmail.com>
2022-12-28 00:38:28 +0600

    Server host to Vercel Hosting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gh0wlz3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri);
@@ -17,6 +18,22 @@ const run = async() => {
     try{
         const loanApplicantsCollection = client.db('Dygnify').collection('loan_applicants');
 
+        // Get all applications
+        app.get('/applications', async(req, res) => {
+            const query = {};
+            const applications = await loanApplicantsCollection.find(query).toArray();
+            res.send(applications);
+        });
+
+        // Get application by ID
+        app.get('/applications/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const application = await loanApplicantsCollection.findOne(query);
+            res.send(application);
+        });
+
+        // Application data sent to mongoDB
         app.post('/addApplication', async(req, res) => {
             const applicationInfo = req.body;
             const result = await loanApplicantsCollection.insertOne(applicationInfo);
@@ -27,9 +44,6 @@ const run = async() => {
     finally{}
 }
 run().catch(err => console.log(err));
-
-
-
 
 app.get('/', (req, res) => {
     res.send('Loan Server Running');
